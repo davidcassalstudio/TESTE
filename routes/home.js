148146571d7a6a17cdb914e45d61849f76574fb2
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:32:40 -0300

    Agora vai

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express();
 
 router.get('/', (req,res,next) => {
-    res.render('HOME')
+    res.send('Foi erro meu')
 })
 
 module.exports = router;

19252875556199ee53bbbd95801e76d4ca209470
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:42:10 -0300

    Module core adicionado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express    = require('express');
 const app        = express();
 const PORT       = process.env.PORT || 5000;
 const homeRouter = require('./routes/home.routes');
-
+const path       = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));

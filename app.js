dbd626f668b38f74c5e2763c52ac113d2ec78ac2
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:40:20 -0300

    Views adicionada

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const PORT       = process.env.PORT || 5000;
 const homeRouter = require('./routes/home.routes');
 
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended: true}));
+
+
 app.use(homeRouter);
 
 app.listen(PORT, () => console.log('Servidor rodando em localhost:5000'));

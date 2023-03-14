2ed419b727e64d2d3f06e764bfb524a8c836c9e1
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:37:39 -0300

    Extensão de rotas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express    = require('express');
 const app        = express();
 const PORT       = process.env.PORT || 5000;
-const homeRouter = require('./routes/home');
+const homeRouter = require('./routes/home.routes');
 
 
 app.use(homeRouter);

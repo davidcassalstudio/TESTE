47934e65c48673ed9ea3e894956041523f2ab13f
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:56:50 -0300

    Agora tem que dar certo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const homeRouter    = require('./routes/home.routes');
 const contatoRouter = require('./routes/contato.routes');
 const loginRouter   = require('./routes/login.routes');
 const path          = require('path');
-const mongoose      = require('mongoose');
+// const mongoose      = require('mongoose');
 
 
 // console.log(process.env.USER)
@@ -14,13 +14,13 @@ const mongoose      = require('mongoose');
 // MongoDB Database Connection
 //mongoose.connect('mongodb://127.0.0.1:27017/aguiasmartech')
 // mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
-mongoose.connect(`mongodb+srv://davidcassal:2wwSRyed2CvH3UpL@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
-    .then(response => {
-        console.log('Conectado com sucesso!');
-    })
-    .catch(error => {
-        console.error('Não foi possível conectar ao banco de dados, tente novamente mais tarde');
-    })
+// mongoose.connect(`mongodb+srv://davidcassal:2wwSRyed2CvH3UpL@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
+//     .then(response => {
+//         console.log('Conectado com sucesso!');
+//     })
+//     .catch(error => {
+//         console.error('Não foi possível conectar ao banco de dados, tente novamente mais tarde');
+//     })
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
@@ -32,7 +32,5 @@ app.use(express.urlencoded({extended: true}));
 app.use(loginRouter);
 app.use(homeRouter);
 app.use(contatoRouter);
-
-app.use(homeRouter);
 
 app.listen(PORT, () => console.log('Servidor rodando em localhost:5000'));

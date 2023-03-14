aca0cd5d74bbd8aaba9856a2f9aec828521c1d19
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:29:28 -0300

    Rota configurada

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express    = require('express');
 const app        = express();
 const PORT       = process.env.PORT || 5000;
-const homeRouter = require('./routes/home.router');
+const homeRouter = require('./routes/home.routes');
 
 
 app.use(homeRouter);

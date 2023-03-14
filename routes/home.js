05777b2183947762496768c779370defbab5a9ec
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:35:55 -0300

    Instalação de libs

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router  = express();
+const mongoose      = require('mongoose');
+
+
+// console.log(process.env.USER)
+// console.log(process.env.PASS)
+
+// MongoDB Database Connection
+//mongoose.connect('mongodb://127.0.0.1:27017/aguiasmartech')
+// mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://davidcassal:2wwSRyed2CvH3UpL@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
+    .then(response => {
+        console.log('Conectado com sucesso!');
+    })
+    .catch(error => {
+        console.error('Não foi possível conectar ao banco de dados, tente novamente mais tarde');
+    })
 
 router.get('/', (req,res,next) => {
     res.send('Foi erro meu')

bd087b13bf270ce9b6b0959278e5fb7d0aadc75a
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:30:54 -0300

    novamente a rota foi configurada

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express    = require('express');
 const app        = express();
 const PORT       = process.env.PORT || 5000;
-const homeRouter = require('./routes/home.routes');
+const homeRouter = require('./routes/home');
 
 
 app.use(homeRouter);

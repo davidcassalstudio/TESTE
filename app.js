1ad6e49d806563589a9fa3bf996dd92f32197fe4
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 23:53:11 -0300

    Teste db

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const path          = require('path');
 // MongoDB Database Connection
 //mongoose.connect('mongodb://127.0.0.1:27017/aguiasmartech')
 // mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
-mongoose.connect(`mongodb+srv://davidcassal:2wwSRyed2CvH3UpL@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect("mongodb+srv://davidcassal:2wwSRyed2CvH3UpL@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority")
     .then(response => {
         console.log('Conectado com sucesso!');
     })
